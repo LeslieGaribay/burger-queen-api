2e9b88bffd5b0da4e3428f3aa8bb3482b6499873
Yennyfer Jarava <yennyfer.jarava92@gmail.com>
2023-09-08 15:38:09 -0500

    Pruebas para la función 'addUser' y 'getUsers'

--- controller/__tests__/users.spec.js
+++ controller/__tests__/users.spec.js
@@ -1,9 +1,56 @@
 const {
   getUsers,
-} = require('../users');
+  addUser,
+} = require('../users-controller');
+
+// describe('getUsers', () => {
+//   it('should get users collection', (done) => {
+//     done();
+//   });
+// });
+
 
 describe('getUsers', () => {
-  it('should get users collection', (done) => {
-    done();
+  it('debería obtener una lista de usuarios', async () => {
+  
+    // Llama a la función getUsers y verifica el resultado
+    const userList = await getUsers();
+
+    // Comprueba si userList es un arreglo
+    expect(Array.isArray(userList)).toBe(true);
+    expect(userList.length).toBeGreaterThan(0);
+
   });
 });
+
+describe('addUser', () => {
+  it('debería agregar un usuario nuevo a la db', async () => {
+    // Datos de ejemplo para el nuevo usuario
+    const newUser = {
+        name: 'Daniel Meza',
+        email: 'd@meza.com',
+        password: 'secret',
+        role: 'user',
+    };
+
+    // Llama a la función addUser y verifica el resultado
+    const result = await addUser(newUser);
+
+    // Comprueba si se generó un _id para el usuario
+    expect(result.insertedId).toBeDefined();
+  });
+
+  it('debería manejar errores al agregar un usuario', async () => {
+    const invalidUser = {
+      name: 'Daniel Meza',
+      password: 'secret',
+    };
+
+    try {
+        await addUser(invalidUser);
+    } catch (error) {
+        expect(error).toBeDefined();
+    }
+});
+});
+
