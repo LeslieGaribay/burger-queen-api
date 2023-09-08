5001e0c2af85d429c922b2bc3451f5542415376c
Yennyfer Jarava <yennyfer.jarava92@gmail.com>
2023-09-08 15:31:20 -0500

    Implementación de la ruta para agregar nuevos usuarios usando la fb addUser

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,31 @@
 const bcrypt = require('bcrypt');
-
 const {
-  requireAuth,
-  requireAdmin,
+    requireAuth,
+    requireAdmin,
 } = require('../middleware/auth');
 
 const {
-  getUsers,
-} = require('../controller/users');
+    getUsers,
+    addUser,
+} = require('../controller/users-controller');
 
 const initAdminUser = (app, next) => {
-  const { adminEmail, adminPassword } = app.get('config');
-  if (!adminEmail || !adminPassword) {
-    return next();
-  }
+    const { adminEmail, adminPassword } = app.get('config');
+    if (!adminEmail || !adminPassword) {
+        return next();
+    }
 
-  const adminUser = {
-    email: adminEmail,
-    password: bcrypt.hashSync(adminPassword, 10),
-    roles: { admin: true },
-  };
+    const adminUser = {
+        email: adminEmail,
+        password: bcrypt.hashSync(adminPassword, 10),
+        roles: { admin: true },
+    };
 
-  // TODO: crear usuaria admin
-  // Primero ver si ya existe adminUser en base de datos
-  // si no existe, hay que guardarlo
+    // TODO: crear usuaria admin
+    // Primero ver si ya existe adminUser en base de datos
+    // si no existe, hay que guardarlo
 
-  next();
+    next();
 };
 
 /*
@@ -56,115 +56,154 @@ const initAdminUser = (app, next) => {
 
 /** @module users */
 module.exports = (app, next) => {
-  /**
-   * @name GET /users
-   * @description Lista usuarias
-   * @path {GET} /users
-   * @query {String} [page=1] Página del listado a consultar
-   * @query {String} [limit=10] Cantitad de elementos por página
-   * @header {Object} link Parámetros de paginación
-   * @header {String} link.first Link a la primera página
-   * @header {String} link.prev Link a la página anterior
-   * @header {String} link.next Link a la página siguiente
-   * @header {String} link.last Link a la última página
-   * @auth Requiere `token` de autenticación y que la usuaria sea **admin**
-   * @response {Array} users
-   * @response {String} users[]._id
-   * @response {Object} users[].email
-   * @response {Object} users[].roles
-   * @response {Boolean} users[].roles.admin
-   * @code {200} si la autenticación es correcta
-   * @code {401} si no hay cabecera de autenticación
-   * @code {403} si no es ni admin
-   */
-  app.get('/users', requireAdmin, getUsers);
+    /**
+     * @name GET /users
+     * @description Lista usuarias
+     * @path {GET} /users
+     * @query {String} [page=1] Página del listado a consultar
+     * @query {String} [limit=10] Cantitad de elementos por página
+     * @header {Object} link Parámetros de paginación
+     * @header {String} link.first Link a la primera página
+     * @header {String} link.prev Link a la página anterior
+     * @header {String} link.next Link a la página siguiente
+     * @header {String} link.last Link a la última página
+     * @auth Requiere `token` de autenticación y que la usuaria sea **admin**
+     * @response {Array} users
+     * @response {String} users[]._id
+     * @response {Object} users[].email
+     * @response {Object} users[].roles
+     * @response {Boolean} users[].roles.admin
+     * @code {200} si la autenticación es correcta
+     * @code {401} si no hay cabecera de autenticación
+     * @code {403} si no es ni admin
+     */
+    app.get('/users', requireAdmin, getUsers);
 
-  /**
-   * @name GET /users/:uid
-   * @description Obtiene información de una usuaria
-   * @path {GET} /users/:uid
-   * @params {String} :uid `id` o `email` de la usuaria a consultar
-   * @auth Requiere `token` de autenticación y que la usuaria sea **admin** o la usuaria a consultar
-   * @response {Object} user
-   * @response {String} user._id
-   * @response {Object} user.email
-   * @response {Object} user.roles
-   * @response {Boolean} user.roles.admin
-   * @code {200} si la autenticación es correcta
-   * @code {401} si no hay cabecera de autenticación
-   * @code {403} si no es ni admin o la misma usuaria
-   * @code {404} si la usuaria solicitada no existe
-   */
-  app.get('/users/:uid', requireAuth, (req, resp) => {
-  });
+    /**
+     * @name GET /users/:uid
+     * @description Obtiene información de una usuaria
+     * @path {GET} /users/:uid
+     * @params {String} :uid `id` o `email` de la usuaria a consultar
+     * @auth Requiere `token` de autenticación y que la usuaria sea **admin** o la usuaria a consultar
+     * @response {Object} user
+     * @response {String} user._id
+     * @response {Object} user.email
+     * @response {Object} user.roles
+     * @response {Boolean} user.roles.admin
+     * @code {200} si la autenticación es correcta
+     * @code {401} si no hay cabecera de autenticación
+     * @code {403} si no es ni admin o la misma usuaria
+     * @code {404} si la usuaria solicitada no existe
+     */
+    app.get('/users/:uid', requireAuth, (req, resp) => {
+    });
 
-  /**
-   * @name POST /users
-   * @description Crea una usuaria
-   * @path {POST} /users
-   * @body {String} email Correo
-   * @body {String} password Contraseña
-   * @body {Object} [roles]
-   * @body {Boolean} [roles.admin]
-   * @auth Requiere `token` de autenticación y que la usuaria sea **admin**
-   * @response {Object} user
-   * @response {String} user._id
-   * @response {Object} user.email
-   * @response {Object} user.roles
-   * @response {Boolean} user.roles.admin
-   * @code {200} si la autenticación es correcta
-   * @code {400} si no se proveen `email` o `password` o ninguno de los dos
-   * @code {401} si no hay cabecera de autenticación
-   * @code {403} si ya existe usuaria con ese `email`
-   */
-  app.post('/users', requireAdmin, (req, resp, next) => {
-    // TODO: implementar la ruta para agregar
-    // nuevos usuarios
-  });
+    /**
+     * @name POST /users
+     * @description Crea una usuaria
+     * @path {POST} /users
+     * @body {String} email Correo
+     * @body {String} password Contraseña
+     * @body {Object} [roles]
+     * @body {Boolean} [roles.admin]
+     * @auth Requiere `token` de autenticación y que la usuaria sea **admin**
+     * @response {Object} user
+     * @response {String} user._id
+     * @response {Object} user.email
+     * @response {Object} user.roles
+     * @response {Boolean} user.roles.admin
+     * @code {200} si la autenticación es correcta
+     * @code {400} si no se proveen `email` o `password` o ninguno de los dos
+     * @code {401} si no hay cabecera de autenticación
+     * @code {403} si ya existe usuaria con ese `email`
+     */
+    app.post('/users', requireAdmin, async (require, response, next) => {
+        // TODO: implementar la ruta para agregar nuevos usuarios
+        // Obtener los datos del nuevo usuario desde el cuerpo de la solicitud
+        const { name, email, password, role } = require.body;
 
-  /**
-   * @name PUT /users
-   * @description Modifica una usuaria
-   * @params {String} :uid `id` o `email` de la usuaria a modificar
-   * @path {PUT} /users
-   * @body {String} email Correo
-   * @body {String} password Contraseña
-   * @body {Object} [roles]
-   * @body {Boolean} [roles.admin]
-   * @auth Requiere `token` de autenticación y que la usuaria sea **admin** o la usuaria a modificar
-   * @response {Object} user
-   * @response {String} user._id
-   * @response {Object} user.email
-   * @response {Object} user.roles
-   * @response {Boolean} user.roles.admin
-   * @code {200} si la autenticación es correcta
-   * @code {400} si no se proveen `email` o `password` o ninguno de los dos
-   * @code {401} si no hay cabecera de autenticación
-   * @code {403} si no es ni admin o la misma usuaria
-   * @code {403} una usuaria no admin intenta de modificar sus `roles`
-   * @code {404} si la usuaria solicitada no existe
-   */
-  app.put('/users/:uid', requireAuth, (req, resp, next) => {
-  });
+        try {
+            // Validar datos
+            if (!name || !email || !password || !role) {
+                return responsepodemos
+                    .status(400)
+                    .json({ message: 'Todos los campos son requeridos' });
+            }
 
-  /**
-   * @name DELETE /users
-   * @description Elimina una usuaria
-   * @params {String} :uid `id` o `email` de la usuaria a modificar
-   * @path {DELETE} /users
-   * @auth Requiere `token` de autenticación y que la usuaria sea **admin** o la usuaria a eliminar
-   * @response {Object} user
-   * @response {String} user._id
-   * @response {Object} user.email
-   * @response {Object} user.roles
-   * @response {Boolean} user.roles.admin
-   * @code {200} si la autenticación es correcta
-   * @code {401} si no hay cabecera de autenticación
-   * @code {403} si no es ni admin o la misma usuaria
-   * @code {404} si la usuaria solicitada no existe
-   */
-  app.delete('/users/:uid', requireAuth, (req, resp, next) => {
-  });
+            // Crear un objeto de nuevo usuario
+            const newUser = {
+                name,
+                email,
+                password,
+                role
+            };
 
-  initAdminUser(app, next);
+            //Llamamos a la funcion addUser para agregar el nuevo usuario a la db
+            const result = await addUser(newUser);
+            // si los datos son validos insertar usuario
+            if (result.insertedId) {
+                return response
+                    .status(201)
+                    .json({ message: "Usuario agregado con éxito" });
+                // si no rechaza peticion indicar el error
+            } else {
+                return response
+                    .status(400)
+                    .json({ message: "Ha fallado la inserción" });
+            }
+        } catch (error) {
+            console.error("Error al agregar al usuario: " + error);
+            return response
+                .status(500)
+                .json({ message: "Error agregando usuario", error: error.message });
+        }
+    });
+
+    /**
+     * @name PUT /users
+     * @description Modifica una usuaria
+     * @params {String} :uid `id` o `email` de la usuaria a modificar
+     * @path {PUT} /users
+     * @body {String} email Correo
+     * @body {String} password Contraseña
+     * @body {Object} [roles]
+     * @body {Boolean} [roles.admin]
+     * @auth Requiere `token` de autenticación y que la usuaria sea **admin** o la usuaria a modificar
+     * @response {Object} user
+     * @response {String} user._id
+     * @response {Object} user.email
+     * @response {Object} user.roles
+     * @response {Boolean} user.roles.admin
+     * @code {200} si la autenticación es correcta
+     * @code {400} si no se proveen `email` o `password` o ninguno de los dos
+     * @code {401} si no hay cabecera de autenticación
+     * @code {403} si no es ni admin o la misma usuaria
+     * @code {403} una usuaria no admin intenta de modificar sus `roles`
+     * @code {404} si la usuaria solicitada no existe
+     */
+    app.put('/users/:uid', requireAuth, (req, resp, next) => {
+    });
+
+    /**
+     * @name DELETE /users
+     * @description Elimina una usuaria
+     * @params {String} :uid `id` o `email` de la usuaria a modificar
+     * @path {DELETE} /users
+     * @auth Requiere `token` de autenticación y que la usuaria sea **admin** o la usuaria a eliminar
+     * @response {Object} user
+     * @response {String} user._id
+     * @response {Object} user.email
+     * @response {Object} user.roles
+     * @response {Boolean} user.roles.admin
+     * @code {200} si la autenticación es correcta
+     * @code {401} si no hay cabecera de autenticación
+     * @code {403} si no es ni admin o la misma usuaria
+     * @code {404} si la usuaria solicitada no existe
+     */
+    app.delete('/users/:uid', requireAuth, (req, resp, next) => {
+    });
+
+    initAdminUser(app, next);
 };
+
+
