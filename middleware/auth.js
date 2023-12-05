f3c0531c77d7beda80112465f2d50121f60a5f87
Yennyferjp <yennyfer.jarava92@gmail.com>
2023-12-05 14:29:10 -0500

    Alineación de Middlewares y Corrección en el Control de Acceso a la Modificación de Órdenes

--- middleware/auth.js
+++ middleware/auth.js
@@ -61,7 +61,7 @@ module.exports.isAdmin = (req) => {
 module.exports.requireAuth = (req, resp, next) => (
   (!module.exports.isAuthenticated(req))
     ? next(new AppError(401, 'No autenticado'))
-    : next()
+    : next(new AppError(403, 'Token inválido'))
 );
 
 module.exports.requireAdmin = (req, resp, next) => (
